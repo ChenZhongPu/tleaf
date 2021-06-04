b9352e32c0df20f9cde4225c2d517d0093716b69
chenzhongpu <chenloveit@gmail.com>
2021-06-04 15:30:24 +0800

    utf-8 encoding for v0.1.1

--- README.md
+++ README.md
@@ -3,9 +3,24 @@ Although `Thymeleaf` can be used in `Spring` with zero-configuration, it require
 To this end, this simple util can help students focus on template engine itself while taking my course [java-ee-swufe](https://github.com/ChenZhongPu/java-ee-swufe).
 
 ## Usage
-```java
-TemplateEngine engine = ThymeleafAppUtil.getTemplateEngine(request.getServletContext());
+
+Firstly, add `Maven` dependency:
+
+```xml
+<dependency>
+    <groupId>info.zhongpu</groupId>
+    <artifactId>tleaf</artifactId>
+    <version>0.1.1</version>
+</dependency>
 ```
+All template files must be put under `webapp/WEB-INF/templates`.
+
+```java
+WebContext wx = ...
+TemplateUtils.process("template_name", wx);
+```
+
+Example project can be found [java-ee-swufe: thymeleaf]().
 
 ## To do
 More flexible configurations will also be provided.
--- src/main/java/tleaf/utils/TemplateUtils.java
+++ src/main/java/tleaf/utils/TemplateUtils.java
@@ -1,15 +1,19 @@
 package tleaf.utils;
 
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class TemplateUtils {
     private static TemplateEngine templateEngine;
 
-    public static TemplateEngine getTemplateEngine(ServletContext context) {
+    private static TemplateEngine getTemplateEngine(ServletContext context) {
         if (templateEngine == null) {
             ServletContextTemplateResolver templateResolver =
                     new ServletContextTemplateResolver(context);
@@ -21,5 +25,30 @@ public class TemplateUtils {
             templateEngine.setTemplateResolver(templateResolver);
         }
         return templateEngine;
+    }
+
+    /**
+     * Response a HTML using template encoded with UTF-8
+     * @param template
+     * @param webContext
+     * @throws ServletException
+     */
+    public static void process(String template, WebContext webContext) throws ServletException {
+        HttpServletResponse response = webContext.getResponse();
+        response.setContentType("text/html;charset=UTF-8");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        try {
+            getTemplateEngine(webContext.getServletContext())
+                    .process(template, webContext, response.getWriter());
+        } catch (Exception e) {
+            try {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            throw new ServletException(e);
+        }
     }
 }
